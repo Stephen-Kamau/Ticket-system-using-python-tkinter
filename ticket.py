3e84b1b83dea3c8164b3e77a75b1024512c28517
Stephen-Kamau <stiveckamash@gmail.com>
2019-11-06 22:40:37 +0300

    Added the labels for the first frame

--- ticket.py
+++ ticket.py
@@ -16,9 +16,11 @@ class guiDefinition:
 		self.root = root
 		#title for the application
 		self.root.title("Ticket app advance")
+		self.configs()
 		self.add_frames()
 		self.top_definition()
-		self.configs()
+		self.add_labels()
+		
 
 
 
@@ -57,7 +59,32 @@ class guiDefinition:
 	def top_definition(self):
 		self.sys_title= Label (self.top,font=('arial',35,'bold'),bd=7,width=35,text='New System dela Advance',justify='center').pack(side=LEFT)
 
-	#add_frames(root)
+	def add_labels(self):#this function is not working as needed...to be continued
+		#...........................................subframes frame.....................................
+		self.lblreceit= Label (self.frametopright,font=('arial',14,'bold'),bd=5,width=31,text='Travelling receits',justify='center').grid(row=0,column=0)
+		self.lblclass1= Label (self.framebottomright,relief='sunken',font=('arial',14,'bold'),width=8,text='Class',justify='center').grid(row=0,column=0)
+		self.lblclass2= Label (self.framebottomright,relief='sunken',textvariable=class1,font=('arial',14,'bold'),width=8,justify='center').grid(row=1,column=0)
+		self.lblticket1= Label (self.framebottomright,relief='sunken',font=('arial',14,'bold'),width=8,text='Ticket',justify='center').grid(row=0,column=1)
+		self.lblticket2= Label (self.framebottomright,relief='sunken',textvariable=fees,font=('arial',14,'bold'),width=8,justify='center').grid(row=1,column=1)
+		self.lbladult1= Label (self.framebottomright,relief='sunken',font=('arial',14,'bold'),width=8,text='Adult',justify='center').grid(row=0,column=2)
+		self.lbladult2= Label (self.framebottomright,relief='sunken',textvariable=adult,font=('arial',14,'bold'),width=8,justify='center').grid(row=1,column=2)
+		self.lblchild1= Label (self.framebottomright,relief='sunken',font=('arial',14,'bold'),width=8,text='Child',justify='center').grid(row=0,column=3)
+		self.lblchild2= Label (self.framebottomright,textvariable=child,relief='sunken',font=('arial',14,'bold'),width=8,justify='center').grid(row=1,column=3)
+		self.lblfrom1= Label (self.framebottomright,relief='sunken',font=('arial',14,'bold'),width=8,text='From',justify='center').grid(row=2,column=1)
+		self.lblfrom2= Label (self.framebottomright,relief='sunken',textvariable=from1,font=('arial',14,'bold'),width=8,justify='center').grid(row=2,column=2)
+		self.lblto1= Label (self.framebottomright,text='To', relief='sunken',font=('arial',14,'bold'),width=8,justify='center').grid(row=3,column=1)
+		self.lblto2= Label (self.framebottomright,relief='sunken',textvariable=to,font=('arial',14,'bold'),width=8,justify='center').grid(row=3,column=2)
+		self.lblprice1= Label (self.framebottomright,text='Price',relief='sunken',font=('arial',14,'bold'),width=8,justify='center').grid(row=3,column=2)
+		self.lblprice2= Label (self.framebottomright,relief='sunken',textvariable=price,font=('arial',14,'bold'),width=8,justify='center').grid(row=3,column=2)
+		self.lblRefNo1= Label (self.framebottomright,relief='sunken',font=('arial',14,'bold'),width=8,text='Ref No',justify='center').grid(row=5,column=0)
+		self.lblRefNo2= Label (self.framebottomright,textvariable=ref,relief='sunken',font=('arial',14,'bold'),width=8,justify='center').grid(row=6,column=0)
+		self.lblTime1= Label (self.framebottomright, relief='sunken',font=('arial',14,'bold'),width=8,text='Time',justify='center').grid(row=5,column=1)
+		self.lblTime2= Label (self.framebottomright,textvariable=time1,relief='sunken',font=('arial',14,'bold'),width=8,justify='center').grid(row=6,column=1)
+		self.lbldate1= Label (self.framebottomright,text='Date',relief='sunken',font=('arial',14,'bold'),width=8,justify='center').grid(row=5,column=2)
+		self.lbldate2= Label (self.framebottomright,textvariable=Date1,relief='sunken',font=('arial',14,'bold'),width=8,justify='center').grid(row=6,column=2)
+		self.lblroute1= Label (self.framebottomright,text='Route',relief='sunken',font=('arial',14,'bold'),width=8,justify='center').grid(row=5,column=3)
+		self.lblroute2= Label (self.framebottomright,relief='sunken',textvariable=route,font=('arial',14,'bold'),width=8,justify='center').grid(row=6,column=3)
+
 	
 
 guiDefinition(root)
